160c027b2ece7600ac947ce806dc71f6b0e2c3de
Teyson <teis@aalbaek-nielsen.dk>
2022-10-20 15:46:20 +0200

    Make testing structure

---
+++ HesehusCampaignEngine/Test/Domain/Campaigns/CampaignUt.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using CampaignEngine.Domain;
+using CampaignEngine.Domain.Campaigns;
+using NUnit.Framework;
+
+namespace Test.Domain.Campaigns
+{
+    [TestFixture]
+    public class CampaignUt
+    {
+        [Test]
+        public void PercentCampaign_CalculatesCorrect()
+        {
+            //Arrange
+            Product p1 = new("1", 100);
+
+            PercentCampaign c1 = new("A", new HashSet<Product>() {p1}, 1, 10);
+            
+            //Act
+            CampaignActivation ca1 = new(c1, new HashSet<Product>() {p1});
+
+            //Assert
+            Assert.That(ca1.Total, Is.EqualTo(90));
+        }
+        
+        [Test]
+        public void DiscountCampaign_CalculatesCorrect()
+        {
+            //Arrange
+            Product p1 = new("1", 100);
+
+            DiscountCampaign c1 = new("A", new HashSet<Product>() {p1}, 1, 50);
+            
+            //Act
+            CampaignActivation CA1 = new(c1, new HashSet<Product>() {p1});
+
+            //Assert
+            Assert.That(CA1.Total, Is.EqualTo(50));
+        }
+        
+        [Test]
+        public void SetPriceCampaign_CalculatesCorrect()
+        {
+            //Arrange
+            Product p1 = new("1", 100);
+
+            SetPriceCampaign c1 = new("A", new HashSet<Product>() {p1}, 1, 5000);
+            
+            //Act
+            CampaignActivation CA1 = new(c1, new HashSet<Product>() {p1});
+
+            //Assert
+            Assert.That(CA1.Total, Is.EqualTo(5000));
+        }
+    }
+}
